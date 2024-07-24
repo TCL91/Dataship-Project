ff6bb2a8a585b806b165d6afb6ce7b5052635317
Tyler Loest <loest.tyler@gmail.com>
2024-07-24 14:36:03 -0500

    testing 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
+import requests
+from io import StringIO
+import streamlit as st
 
 st.title('Mental Health in the TechField 2016')
 
+
+
+def load_original_data():
+    url = 'https://raw.githubusercontent.com/TCL91/Mental-Health-Project/main/data/processed/Analysis.csv'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return pd.read_csv(StringIO(response.text))
+    else:
+        st.error("Failed to load data from GitHub.")
+        return None
+    
+mh2016 = load_original_data()
+
+# -----------------------------------------------------------------------------
+# Draw the actual page
+
+# Set the title that appears at the top of the page.
+'''
+#  Mental Health dashboard
+
+Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
+notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
+But it's otherwise a great (and did I mention _free_?) source of data.
+'''
